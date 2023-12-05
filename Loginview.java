0ef05897bf05bf1880e5eb59c44d04af6af64622
Rolando Espin <146813171+LandoEspin@users.noreply.github.com>
2023-12-06 02:52:31 +0800

    Update Loginview.java
    kulang nakog tulog

--- Loginview.java
+++ Loginview.java
@@ -1,1 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
 
+class Loginview{
+  JFrame frame = new JFrame();
+  frame.setSize(500, 500);
+  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  frame.setLocationRelativeTo(null)
+  frame.setLayout(null);
+  frame.setVisible(true);
+
+  JPanel loginPanel = new JPanel();
+  loginPanel.setBackground(Color.gray);
+	loginPanel.setOpaque(false);
+	loginPanel.setBounds(0, 0, 500, 500);
+	frame.add(loginPanel);
+
+
+}
+
+  JLabel wellabel = new JLabel("WELCOME);
+  
+
+}
