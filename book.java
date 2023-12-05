81e420b1a72b604778b888c4ac297709fb2f1a02
Rolando Espin <146813171+LandoEspin@users.noreply.github.com>
2023-12-05 22:43:31 +0800

    Update book.java

--- book.java
+++ book.java
@@ -1,5 +1,26 @@
 public class book {
 	public static void main(String[] args) {
 		
+	}public class bookLogin {
+	public static void frame(JFrame frame){
+		frame.setSize(600,600);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(null);
+		frame.setVisible(true);
+		
+
+		
 	}
+	public static void addButton(JFrame frame, JButton button, int x int y, int width, int height){
+
+		button.setBounds(x,y,width,height);
+		frame.add(button);
+	}	
+
+	public static void addLabel(JFrame frame, JButton button, int x int y, int width, int height){
+		label.setBounds(x,y,width,height);
+		frame.add(label);
+	}
+}
+
 }
