88093c618e3da325031b9bc3d1a70dea3d5eeb8f
Rolando Espin <146813171+LandoEspin@users.noreply.github.com>
2023-12-06 03:27:43 +0800

    Update Loginview.java
    boy

--- Loginview.java
+++ Loginview.java
@@ -2,15 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 
 class Loginview{
-  JFrame frame = new JFrame();
-  frame.setSize(500, 500);
-  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-  frame.setLocationRelativeTo(null)
-  frame.setLayout(null);
-  frame.setVisible(true);
+ 	JFrame frame = new JFrame();
+  	frame.setSize(500, 500);
+ 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 	frame.setLocationRelativeTo(null)
+  	frame.setLayout(null);
+  	frame.setVisible(true);
 
-  JPanel loginPanel = new JPanel();
-  loginPanel.setBackground(Color.gray);
+  	JPanel loginPanel = new JPanel();
+  	loginPanel.setBackground(Color.gray);
 	loginPanel.setOpaque(false);
 	loginPanel.setBounds(0, 0, 500, 500);
 	frame.add(loginPanel);
@@ -18,7 +18,34 @@ class Loginview{
 
 }
 
-  JLabel wellabel = new JLabel("WELCOME);
+  	JLabel wellabel = new JLabel("WELCOME);
+	wellabel.setBound(320, 50, 200, 50);
+	loginPanel.add(wellabel);
+
+	JLabel emaillabel = new JLabel("E-mail Address");
+	emaillabel.setBounds(260, 130, 80, 20);
+	loginPanel.add(emaillabel);
+
+	JTextField emailfield = new JTextField();
+	emailfield.setBounds(260, 160, 150, 40);
+	loginPanel.add(emailfield);
+
+	JLabel passlabel = new JLabel("Password");
+	passlabel.setBounds(260, 210, 80, 20);
+	loginPanel.add(passlabel);
+
+	JTextField passfield = new JTextField();
+	passfield.setBounds(260, 240, 150, 40);
+	loginPanel.add(passfield);
+
+	JButton logbut = new JButton("LOGIN");
+	logbut.setBounds(260, 360, 150, 40);
+	loginPanel.add(logbut);
+
+	JButton regbut = new JButton("REGISTER");
+	regbut.setBounds(260, 410, 150, 40);
+	loginPanel.add(regbut);
+	
   
 
 }
