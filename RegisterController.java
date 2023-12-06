5aa0037dae7d802acf38afa01edaa62ae01f008a
clesterbadz <146814262+clesterbadz@users.noreply.github.com>
2023-12-06 14:08:40 +0800

    RegisterController.java

---
+++ RegisterController.java
@@ -0,0 +1,14 @@
+package oop.project;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class RegisterController implements ActionListener{
+
+  RegisterController(){
+
+    //diri ba dapat nako i-butang ang actionListener Chairman? or what? ahhaahaha di ko sure gud.
+  }
+  
+}
