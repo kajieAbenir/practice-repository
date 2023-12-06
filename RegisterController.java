e20cfe81b2ae65d31ea62a54d8abbf2facb8257b
clesterbadz <146814262+clesterbadz@users.noreply.github.com>
2023-12-07 00:18:36 +0800

    RegisterController.java

--- RegisterController.java
+++ RegisterController.java
@@ -1,14 +1,17 @@
 package oop.project;
 
-import javax.swing.*;
-import java.awt.*;
 import java.awt.event.*;
 
 public class RegisterController implements ActionListener{
 
-  RegisterController(){
-
-    //diri ba dapat nako i-butang ang actionListener Chairman? or what? ahhaahaha di ko sure gud.
+  @override
+  public static void actionPerformed(ActionEvent e){
+    clearFrame();
+    if(e.getSource() == registerButton){
+    //asa mani padung chairman? i mean unsa may dapat mo gawas? dapat ba registered successfully with another panel?
+    }else if(e.getSource() == cancelButton){
+    //ug kani pud unsa man? hehe? if ma cancel unsa mam dapat mo gawas?
+    }
   }
   
 }
