380de1a22c874e1ce738070ba0d3af70f77a9981
kajieAbenir <146812869+kajieAbenir@users.noreply.github.com>
2023-12-06 11:50:40 +0800

    Update bookLogin.java
    updated ni espin

--- bookLogin.java
+++ bookLogin.java
@@ -1,9 +1,18 @@
 public class bookLogin {
 	public static void frame(JFrame frame){
 		frame.setSize(600,600);
-		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocationRelativeTo(null);
 		frame.setLayout(null);
+		frame.setVisible(true);
+	}
+	
+	public static void addButton(JFrame frame, JButton button, int x int y, int width, int height){
+		button.setBounds(x,y,width,height);
+		frame.add(button);
+	}	
+	
+	public static void addLabel(JFrame frame, JButton button, int x int y, int width, int height){
+		label.setBounds(x,y,width,height);
+		frame.add(label);
 	}
 }
