795319daa482ca5914b29054d5fb818c05c6224c
clesterbadz <146814262+clesterbadz@users.noreply.github.com>
2023-12-07 00:16:44 +0800

    Update RegisterView.java
    BADZ

--- RegisterView.java
+++ RegisterView.java
@@ -42,24 +42,14 @@ public class RegisterView implements ActionListener{
     addComponents.addTextField(registerPanel, passwordTextField, 40, 420, 300, 40);
     registerPanel.add(passwordTextField);
 
-    JButton registerButton - new JButton();
+    JButton registerButton - new JButton("REGISTER");
     addComponents.addButton(registerPanel, registerButton, 40, 490, 300, 40);
     registerButton.addActionListener(this);
     registerPanel.add(registerButton);
   
-    JButton cancelButton - new JButton();
+    JButton cancelButton - new JButton("CANCEL");
     addComponents.addButton(registerPanel, cancelButton, 40, 490, 300, 40);
     cancelButton.addActionListener(this);
     registerPanel.add(cancelButton);
-  }
-
-  @override
-  public static void actionPerformed(ActionEvent e){
-    clearFrame();
-    if(e.getSource() == registerButton){
-      //asa mani padung chairman? i mean unsa may dapat mo gawas? dapat ba registered successfully with another panel?
-    }else if(e.getSource() == cancelButton){
-      //ug kani pud unsa man? hehe? if ma cancel unsa mam dapat mo gawas?
-    }
   }
 }
