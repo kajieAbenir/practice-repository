b337137457cf80a659e9cabd05892f409af4e4c8
clesterbadz <146814262+clesterbadz@users.noreply.github.com>
2023-12-06 11:44:14 +0800

    RegisterView.java

---
+++ RegisterView.java
@@ -0,0 +1,16 @@
+package oop.project;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class RegisterView implements ActionListener{
+
+  Components addComponents = new Components();
+  
+  RegisterView(){
+    
+    addComponents.addPanel(mainFrame, )
+  }
+
+}
