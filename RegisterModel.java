3319d98462b8f9741a29107c611207d51e492b36
clesterbadz <146814262+clesterbadz@users.noreply.github.com>
2023-12-07 00:27:02 +0800

    RegisterModel.java
    badz

---
+++ RegisterModel.java
@@ -0,0 +1,11 @@
+package oop.project;
+
+import java.util.*;
+
+public class RegisterModel extends Components {
+  
+  RegisterModel(){
+    //unya nani nako buhaton ha, unahon sa to nako ang Controller.
+  }
+  
+}
